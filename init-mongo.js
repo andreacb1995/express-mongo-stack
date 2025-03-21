eaed25c948031e7624391b096e6545f9e21926ea
andreacb1995 <andrea.carballido.ballesteros@gmail.com>
2025-03-21 17:38:32 +0100

    imagen dockerhub

--- init-mongo.js
+++ init-mongo.js
@@ -1,11 +1,11 @@
-db = db.getSiblingDB('proyectofinal1'); // Cambia esto si tu base de datos tiene otro nombre
+db = db.getSiblingDB('proyectofinal1'); 
 
 // Verificar si la colección 'users' ya existe
-if (db.users.countDocuments() === 0) {
+if (db.usuarios.countDocuments() === 0) {
     // Crear la colección y agregar un usuario de prueba
-    db.users.insertOne({
+    db.usuarios.insertOne({
         name: "admin",
-        password: "1234" // Dato de prueba
+        password: "1234" 
     });
     print("Usuario de prueba agregado");
 } else {
