875c64f1148277fe212be6236ef27b66a4df5e08
andreacb1995 <andrea.carballido.ballesteros@gmail.com>
2025-03-21 17:20:12 +0100

    cambio puerto server.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 // URL de conexión a MongoDB (usando la variable de entorno definida en docker-compose)
 const mongoURI = process.env.MONGO_URI || "mongodb://localhost:27017/proyectofinal1";
